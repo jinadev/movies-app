4e7f8cb1587102d632b058fd48f80c3b45abba2f
jinadev <jinpnr@gmail.com>
2019-06-25 01:06:12 +0530

    Basic routing added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import MoviesList from './containers/MoviesList';
+import ActorsList from './containers/ActorsList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Route exact path='/' component={MoviesList} />
+        <Route path='/movies' component={MoviesList} />
+        <Route path='/actors' component={ActorsList} />
+      </Router>
     </div>
   );
 }
---
+++ src/containers/MoviesList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class MoviesList extends React.Component {
+    render() {
+        return (
+            <h1> Movies List</h1>
+        )
+    }
+}
